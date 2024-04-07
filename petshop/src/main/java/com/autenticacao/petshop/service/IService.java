0593c9c5674cd185c6bf902dd4229abeec9a15e9
vmC0sta <duda.cv2404@gmail.com>
2024-04-07 17:11:40 -0300

    Adicionado contrato/interface para o Service

--- petshop/src/main/java/com/autenticacao/petshop/service/IService.java
+++ petshop/src/main/java/com/autenticacao/petshop/service/IService.java
@@ -1,2 +1,12 @@
-package com.autenticacao.petshop.service;public interface IService {
+package com.autenticacao.petshop.service;
+
+import java.util.List;
+
+public interface IService<T> {
+
+    public List<T> findAll();
+    public T findById(Long id);
+    public T save(T t);
+    public T update(T newt, Long id);
+    public void delete(Long id);
 }
