6897bfe7f7dd6874b1541868bf11771a2d1a162d
vmC0sta <duda.cv2404@gmail.com>
2024-04-07 17:10:40 -0300

    Adicionado gerenciador de exceções customizadas

--- petshop/src/main/java/com/autenticacao/petshop/exception/handle/CustomExceptionHandler.java
+++ petshop/src/main/java/com/autenticacao/petshop/exception/handle/CustomExceptionHandler.java
@@ -1,2 +1,25 @@
-package com.autenticacao.petshop.exception.handle;public class CustomExceptionHandler {
+package com.autenticacao.petshop.exception.handle;
+
+import com.autenticacao.petshop.exception.ResourceAlreadyExistsException;
+import com.autenticacao.petshop.exception.ResourceNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class CustomExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<Object> handleResourceNotFoundException(ResourceNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(ResourceAlreadyExistsException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<Object> handleResourceAlreadyExistsException(ResourceAlreadyExistsException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
 }
