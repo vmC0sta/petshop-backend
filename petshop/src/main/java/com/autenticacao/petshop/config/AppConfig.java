4fb26fab4ad42740aefba54bba123c863aea2072
vmC0sta <duda.cv2404@gmail.com>
2024-04-07 16:59:41 -0300

    Adicionado Bean ModelMapper

--- petshop/src/main/java/com/autenticacao/petshop/config/AppConfig.java
+++ petshop/src/main/java/com/autenticacao/petshop/config/AppConfig.java
@@ -1,2 +1,13 @@
-package com.autenticacao.petshop.config;public class AppConfig {
+package com.autenticacao.petshop.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 }
